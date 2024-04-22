3c6497c4950a7fa9b40aade516bb7930f93c900d
Sukencio <94089568+sukencio@users.noreply.github.com>
2024-04-22 10:06:01 -0600

    Update Consulta.cs
    Parte de Consultas

--- Veterinaria.DAL/Consulta.cs
+++ Veterinaria.DAL/Consulta.cs
@@ -1,12 +1,35 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Veterinaria.DAL
 {
-    internal class Consulta
+    [Table("Consulta")]
+
+    public class Consulta
     {
-    }
+        [Key]
+        [Required]
+        public int IdConsulta { get; set; }
+        [Required]
+    
+        public DateTime DateTimeValue { get; set; }
+        [Required]
+        public string Motivo { get; set; }
+        [Required]
+    
+        public string Diagnostico { get; set; }
+        [Required]
+    
+        public string Tratamiento { get; set; }
+        [Required]
+    
+        public string Medicamentos { get; set; }
+        
+
+}
 }
