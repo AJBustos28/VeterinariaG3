30f42cb831d6e0b27c1d455bf754c30be441c7dd
Sukencio <94089568+sukencio@users.noreply.github.com>
2024-04-24 14:34:55 -0600

    Update VeterinariaDbContext.cs

--- Veterinaria.DAL/VeterinariaDbContext.cs
+++ Veterinaria.DAL/VeterinariaDbContext.cs
@@ -1,13 +1,8 @@
-﻿using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 
 namespace Veterinaria.DAL
 {
-    public class VeterinariaDbContext : DbContext 
+    public class VeterinariaDbContext : DbContext
     {
         public VeterinariaDbContext() { }
 
@@ -16,16 +11,13 @@ namespace Veterinaria.DAL
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             base.OnConfiguring(optionsBuilder);
+
             optionsBuilder.UseSqlServer();
         }
 
-        public virtual DbSet<Veterinario> Veterinario    { get; set; }
-
+        public virtual DbSet<Veterinario> Veterinario { get; set; }
         public virtual DbSet<Cliente> Clientes { get; set; }
-
         public virtual DbSet<Mascota> Mascotas { get; set; }
-
         public virtual DbSet<Consulta> Consulta { get; set; }
-
     }
 }
