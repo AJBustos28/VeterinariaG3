a7f21155c717dd6345a00178c3c86235144bedd0
Aaron Bustos <aaronj.bustos@gmail.com>
2024-04-23 21:45:03 -0600

    autenticacion (incompleto)

--- Veterinaria.DAL/Cliente.cs
+++ Veterinaria.DAL/Cliente.cs
@@ -1,12 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Veterinaria.DAL
 {
-    internal class Cliente
+    [Table("Clientes")]
+    public class Cliente
     {
+        [Key]
+        [Required]
+        public int IdCliente { get; set; }
+
+        [Required]
+        public string Nombre { get; set; }
+
+        [Required]
+        public string Direccion { get; set; }
+
+        [Required]
+        public string Telefono { get; set; }
+
+        [Required]
+        public string Correo { get; set; }
+
     }
 }
---
+++ Veterinaria.DAL/VeterinariaDbContext.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Veterinaria.DAL
+{
+    public class VeterinariaDbContext : DbContext 
+    {
+        public VeterinariaDbContext() { }
+
+        public VeterinariaDbContext(DbContextOptions<VeterinariaDbContext> options) : base(options) { }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer();
+        }
+
+        public virtual DbSet<Veterinario> Veterinario    { get; set; }
+
+        public virtual DbSet<Cliente> Clientes { get; set; }
+
+        public virtual DbSet<Mascota> Mascotas { get; set; }
+
+        public virtual DbSet<Consulta> Consulta { get; set; }
+
+    }
+}
